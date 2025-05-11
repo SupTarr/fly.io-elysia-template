0de3627cecb96d40a45bc7c8cad70cff607c8e47
supakrit.c <supakrit.c@arise.tech>
2025-05-12 01:42:52 +0700

    feat: deploy vercel

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const elysia_1 = require("elysia");
-const app = new elysia_1.Elysia().get("/hello", () => "Hello Elysia");
-exports.default = app.handle;
