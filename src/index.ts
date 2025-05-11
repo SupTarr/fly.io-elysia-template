da91248bbeba0288c00bb70e50d4c67db120f05b
supakrit.c <supakrit.c@arise.tech>
2025-05-12 01:15:05 +0700

    feat: add elysia vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia");
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export default app.fetch;
