15f18c28dd880fc15af15f3b25f77f08dc4325be
supakrit.c <supakrit.c@arise.tech>
2025-05-12 13:49:39 +0700

    remove .js code

--- api/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const elysia_1 = require("elysia");
-const node_1 = require("@elysiajs/node");
-const app = new elysia_1.Elysia({ adapter: (0, node_1.node)() })
-    .get('/', () => 'Hello Node!');
-exports.default = app.fetch;
